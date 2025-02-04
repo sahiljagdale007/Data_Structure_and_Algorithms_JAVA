4605cd172230ddd920d6cf232cc258e940f91630
Sahiljagdale07 <sahilashokjagdale@gmail.com>
2025-02-04 19:54:45 +0530

    init commit

--- src/EIGHT_recursion/Binary_Stiring_No_One.java
+++ src/EIGHT_recursion/Binary_Stiring_No_One.java
@@ -1,7 +1,7 @@
 package EIGHT_recursion;
 
 public class Binary_Stiring_No_One {
-    public static void conesecutiveOne(int n,int lastPlace, String str) {
+    public static void conesecutiveOne(int n ,int lastPlace, String str) {
         if(n==0) {
             System.out.println(str);
             return;
