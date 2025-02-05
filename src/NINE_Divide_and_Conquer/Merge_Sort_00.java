c357a0e953aa207fccaaf8d18399a9fbbd640792
Sahiljagdale07 <sahilashokjagdale@gmail.com>
2025-02-05 23:45:59 +0530

    init commit

---
+++ src/NINE_Divide_and_Conquer/Merge_Sort_00.java
@@ -0,0 +1,9 @@
+//Time complexity of this algorithm is O(log n)
+package NINE_Divide_and_Conquer;
+
+public class Merge_Sort_00 {
+    public static void main(String[]args) {
+        int arr[] = {6,3,9,5,2,8};
+
+    }
+}
