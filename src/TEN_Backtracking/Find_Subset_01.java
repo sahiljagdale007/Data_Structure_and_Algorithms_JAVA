42f8ec36e5cada0aa73c071b97c800e85a8d9140
Sahil <sahilashokjagdale@gmail.com>
2025-02-18 18:19:58 +0530

    question added

---
+++ src/TEN_Backtracking/Find_Subset_01.java
@@ -0,0 +1,27 @@
+package TEN_Backtracking;
+
+//REMEMBER: Backtracking concept is happening in Call Stack.
+
+public class Find_Subset_01 {
+    public static void findSubset(String str, String ans, int i) {
+        //base case
+        if (i == str.length()) {
+            if (ans.length() == 0) {
+                System.out.println("null");
+            } else {
+                System.out.println(ans);
+            }
+            return;
+        }
+
+        //yes choice
+        findSubset(str, ans + str.charAt(i), i + 1);
+        //no choice
+        findSubset(str, ans, i + 1);
+    }
+
+    public static void main(String[] args) {
+        String str = "abc";
+        findSubset(str, "", 0);
+    }
+}
