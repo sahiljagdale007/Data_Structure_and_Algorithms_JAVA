0134ec9be8144b66ed2d4e49a06400427832e973
Sahil <sahilashokjagdale@gmail.com>
2025-03-13 21:43:03 +0530

    init commit

---
+++ src/ELEVEN_ArrayList/Sorting_Arraylist_05.java
@@ -0,0 +1,18 @@
+package ELEVEN_ArrayList;
+
+import java.util.*;
+public class Sorting_Arraylist_05 {
+    public static void main(String[]args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(5);
+        list.add(8);
+        list.add(10);
+        list.add(1);
+        list.add(7);
+
+        Collections.sort(list);
+        System.out.println(list);
+        Collections.sort(list,Collections.reverseOrder());
+        System.out.println(list);
+    }
+}
